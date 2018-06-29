ff9f2a74ef73b2d7386dcd70755aac8ba47c7478
Eliot Charette <eliotcharette@gmail.com>
2018-06-29 10:21:53 -0700

    Test for if a word matches the inputted word

--- WordCounter.Tests/ModelsTests/WordCounterTest.cs
+++ WordCounter.Tests/ModelsTests/WordCounterTest.cs
@@ -8,10 +8,23 @@ namespace WordCounterName.Tests
   [TestClass]
   public class WordCounterTest
   {
+    // [TestMethod]
+    // public void WordCounterScore_FindWordOnce_Equal()
+    // {
+    //   WordCounterScore newWord = new WordCounterScore();
+    //   int result = newWord.FindWord("hello");
+    //   int score = newWord.GetScore();
+    //   Assert.AreEqual(score, 0);
+    // }
     [TestMethod]
-    public void WordCounterScore_FindWordOnce_Equal()
+    public void Matching_ReturnInput_String()
     {
-      
+      string input = "hello";
+      WordCounterScore newWord = new WordCounterScore(input);
+
+      string result = newWord.Matching();
+
+      Assert.AreEqual(input, result);
     }
   }
 }
--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -5,13 +5,42 @@ namespace WordCounterName
 {
   public class WordCounterScore
   {
+  //  public int numberOfTimes = 0;
+    private string _input;
 
-  }
-  public class Program
-  {
-    public static void Main()
+    public WordCounterScore (string input)
     {
-
+      _input = input;
     }
+    public string Matching()
+    {
+      return _input;
+    }
+
+    // public void FindWord(string inputWord)
+    // {
+    //   foreach(string inputWord in userWord)
+    //   {
+    //     if(inputWord == userWord)
+    //     {
+    //       numberOfTimes++;
+    //     }
+    //   }
+    // }
+    // public int GetScore()
+    // {
+    //   return numberOfTimes;
+    // }
   }
+  // public class Program
+  // {
+  //   public static void Main()
+  //   {
+  //     WordCounterScore newWord = new WordCounterScore();
+  //     Console.WriteLine("Enter a word and I will tell you how many times it occurs in your sentence: ");
+  //     string userWord = Console.ReadLine();
+  //     userWord = userWord.ToLower();
+  //
+  //   }
+  // }
 }
