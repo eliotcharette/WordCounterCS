013d941518d042136828f95a660c739a25f052b5
Eliot Charette <eliotcharette@gmail.com>
2018-06-29 15:44:26 -0700

    Add correct logic but its all in main

--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -6,35 +6,37 @@ namespace WordCounterName
   public class WordCounterScore
   {
     public int numberOfTimes = 0;
+
     public WordCounterScore()
     {
 
     }
-    public void FindWord(string userWord)
-    {
-
-
-      if(userWord == "hello")
-      {
-        numberOfTimes++;
-      }
-    }
-    public int GetScore()
-    {
-      return numberOfTimes;
-    }
+    
   }
   public class Program
   {
     public static void Main()
     {
-      WordCounterScore newWord = new WordCounterScore();
+      //WordCounterScore newWord = new WordCounterScore();
+      int numberOfTimes = 0;
+
       Console.WriteLine("Enter a word");
       string userWord = Console.ReadLine();
       userWord = userWord.ToLower();
-      newWord.FindWord(userWord);
-      int score = newWord.GetScore();
-      Console.WriteLine(score);
+      Console.WriteLine("Your word is: " + userWord);
+
+      Console.WriteLine("Now enter and sentence and I will tell you how many times your word occurs: ");
+      string userPhrase = Console.ReadLine();
+      userPhrase = userPhrase.ToLower();
+      string[] separators = {",", ".", "!", "?", ";", ":", " "};
+      string[] words = userPhrase.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+      foreach (string word in words)
+      if(word == userWord)
+      {
+        numberOfTimes++;
+      }
+      Console.WriteLine(numberOfTimes);
     }
   }
 }
