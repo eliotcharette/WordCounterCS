f11ca49645f4cf0ee3fd610217cf0e190fcbb242
Eliot Charette <eliotcharette@gmail.com>
2018-06-29 14:04:25 -0700

    Show that little progress has been made

--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -6,59 +6,35 @@ namespace WordCounterName
   public class WordCounterScore
   {
     public int numberOfTimes = 0;
-
     public WordCounterScore()
     {
 
     }
-    public void Run()
+    public void FindWord(string userWord)
     {
-      Console.WriteLine("Enter a word and I will tell you how many times it occurs in your sentence: ");
-      string userWord = Console.ReadLine();
-      userWord = userWord.ToLower();
-      // string[] words = userWord.Split(new string[]{" "}, StringSplitOptions.RemoveEmptyEntries);
 
-      // foreach(userWord in words)
-      // {
-      }
-      public int FindWord(string userWord)
+
+      if(userWord == "hello")
       {
-        if(userWord == "hello")
-        {
-          numberOfTimes++;
-          Console.WriteLine(numberOfTimes);
-        }
-        else
-        {
-          Console.WriteLine(10);
-        }
+        numberOfTimes++;
       }
-    // }
-    // public int FindWord(string userWord)
-    // {
-    //     if(userWord == "hello")
-    //     {
-    //       numberOfTimes++;
-    //       return numberOfTimes;
-    //     }
-    //     else
-    //     {
-    //       return 10;
-    //     }
-    // }
-    // public int GetScore()
-    // {
-    //   return numberOfTimes;
-    // }
+    }
+    public int GetScore()
+    {
+      return numberOfTimes;
+    }
   }
   public class Program
   {
     public static void Main()
     {
       WordCounterScore newWord = new WordCounterScore();
-      newWord.Run();
-
-
+      Console.WriteLine("Enter a word");
+      string userWord = Console.ReadLine();
+      userWord = userWord.ToLower();
+      newWord.FindWord(userWord);
+      int score = newWord.GetScore();
+      Console.WriteLine(score);
     }
   }
 }
